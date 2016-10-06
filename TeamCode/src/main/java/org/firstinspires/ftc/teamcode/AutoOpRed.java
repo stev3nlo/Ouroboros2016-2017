45add98baf0105a9b3213afc0c6a51cff373633e
mnanez26 <mormorswim@gmail.com>
2016-10-06 09:35:04 -0500

    Started AutoOp

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoOpRed.java
@@ -0,0 +1,23 @@
+package org.firstinspires.ftc.teamcode;
+
+/**
+ * Created by Morgannanez on 10/6/16.
+ */
+import org.firstinspires.ftc.teamcode.Libraries.MyOpMode;
+import org.firstinspires.ftc.teamcode.Libraries.RPMStabilizer;
+
+public class AutoOpRed extends MyOpMode {
+
+public void runOpMode() {
+    moveForwards(1.0);
+    moveToWhiteLine(1.0);
+    turnRightToWhiteLine(1.0);
+    setServoDropperPosition(1.0);
+    openServoDropper();
+    closeServoDropper();
+
+
+}
+
+
+}
