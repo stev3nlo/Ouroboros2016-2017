35e90e2bd14f49ea8e02179eaf2fa65163db3c92
stev3nlo <stev3nlo17@gmail.com>
2016-10-20 15:56:47 -0500

    RPM Stabilizer test (Not done)

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RPMStabilizerTest.java
@@ -0,0 +1,22 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import org.firstinspires.ftc.teamcode.Libraries.MyOpMode;
+import org.firstinspires.ftc.teamcode.Libraries.RPMStabilizer;
+
+/**
+ * Created by Steven on 10/20/2016.
+ */
+@Autonomous(name="RPM Stabilizer Test", group="Test")
+public class RPMStabilizerTest extends MyOpMode {
+
+	double power;
+	double currPower;
+	double currRPM;
+	double goalRPM;
+
+	public void runOpMode() {
+		power = RPMStabilizer.returnPowerToTry(currPower, currRPM, goalRPM);
+	}
+}
