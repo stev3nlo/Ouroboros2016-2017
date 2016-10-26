a1bbe894941de0fff5a4ba1502211d97088aa4c5
mnanez26 <mormorswim@gmail.com>
2016-10-26 08:08:46 -0500

    Completed rangeTest class and Completed SensorMRRange class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Libraries/SensorMRRange.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Libraries/SensorMRRange.java
@@ -17,13 +17,25 @@ import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 public class SensorMRRange extends MyOpMode {
 
 	I2cDevice rangeSensor;
+	I2cAddr rangeAddress = new I2cAddr(0x14); //Default I2C address for MR Range (7-bit)
+	int rangeRegisterStart = 0x04; //Register to start reading
+	int rangeArrayLength = 2; //Number of byte to read
+	byte[] rangeCache; //The read will return an array of bytes.
+	I2cDeviceSynch rangeReader;
+
 
 	public SensorMRRange(I2cDevice rangeSensor) {
 		this.rangeSensor = rangeSensor;
+		rangeReader = new I2cDeviceSynchImpl(rangeSensor, rangeAddress, false);
+		rangeReader.engage();
+		rangeCache = rangeReader.read(rangeRegisterStart, rangeArrayLength);
 	}
 
-	public double getDistance() {
-		//	return rangeSensor.getDistance(DistanceUnit.INCH);
-		return 0;
+	public double getOpticDistance() {
+		return (double) (rangeCache[1] & 0xFF);
+	}
+
+	public double getUltraSonicDistance(){
+		return (double) (rangeCache[0] & 0xFF);
 	}
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RangeTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RangeTest.java
@@ -5,6 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import org.firstinspires.ftc.teamcode.Libraries.MyOpMode;
 import org.firstinspires.ftc.teamcode.Libraries.SensorMRRange;
 
+
 /**
  * Created by Morgannanez on 10/25/16.
  */
@@ -18,7 +19,17 @@ public class RangeTest extends MyOpMode {
     public void initialize()
     {
         rangeTest = new SensorMRRange(hardwareMap.i2cDevice.get("rangeTest"));
+    }
 
+    public void runOpMode() throws InterruptedException {
+        initialize();
+        waitForStart();
+        while (opModeIsActive())
+        {
+            telemetry.addData("Ultra Sonic", rangeTest.getUltraSonicDistance());
+            telemetry.addData("Optic Distance", rangeTest.getOpticDistance());
+            telemetry.update();
+        }
     }
 
 }
