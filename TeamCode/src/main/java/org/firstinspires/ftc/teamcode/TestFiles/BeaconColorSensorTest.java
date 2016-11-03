3ba95568151db1b68b38226dad548693297ba297
stev3nlo <stev3nlo17@gmail.com>
2016-11-03 08:44:04 -0500

    added servo to beacon test
    gives null pointer exception

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestFiles/BeaconColorSensorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestFiles/BeaconColorSensorTest.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode.TestFiles;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.teamcode.Libraries.MyOpMode;
 import org.firstinspires.ftc.teamcode.Libraries.SensorMRColor;
@@ -12,24 +13,26 @@ import org.firstinspires.ftc.teamcode.Libraries.SensorMRColor;
 public class BeaconColorSensorTest extends MyOpMode {
 
 	SensorMRColor colorB;
-
+	Servo servoBeaconPusher;
 
 	public void initialize(){
 		colorB = new SensorMRColor(hardwareMap.colorSensor.get("colorB"));
 
 		colorB.sensorSetup(0x2c);
 		colorB.lightOff();
+
+		servoBeaconPusher = hardwareMap.servo.get(("servoBeaconPusher"));
+		resetButtonPress();		//this is where it returns null pointer exception
 	}
 
 	public void runOpMode() throws InterruptedException {
 		initialize();
 		waitForStart();
-		while (opModeIsActive()) {
-			telemetry.addData("Color Center Values", colorB);
-			telemetry.addData("Color Center Beacon", colorB.beaconColor());
-			telemetry.update();
+		telemetry.addData("Color Center Values", colorB);
+		telemetry.addData("Color Center Beacon", colorB.beaconColor());
+		telemetry.update();
 
-			idle();
-		}
+//		pushButton("Blue");
+//		resetButtonPress();
 	}
 }
