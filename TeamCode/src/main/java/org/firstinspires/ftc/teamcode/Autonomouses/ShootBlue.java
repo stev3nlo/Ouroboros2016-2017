d0afd2e856236d1b56375c1646184151648f197a
mnanez26 <mormorswim@gmail.com>
2017-01-06 16:04:24 -0600

    added shoot only auto classes
    1: added auto class (ShootRed, ShootBlue) that will move straight then
    shoot.
    - the in the both classes, the robot must be set up backwards in order
    to get the shooter on the right side to shoot

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomouses/ShootBlue.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode.Autonomouses;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import org.firstinspires.ftc.teamcode.Libraries.MyAutonomous;
+
+/**
+ * Created by Morgannanez on 1/6/17.
+ */
+
+@Autonomous(name="Partner Blue Shoot", group="Auto")
+public class ShootBlue extends MyAutonomous {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        super.runOpMode();
+        waitForStart();
+        initCurtime();
+        initTime = getCurTime();
+        simpleStabilizingLoop(1.0);
+        moveWithEncoders(-.8, 3700);
+        simpleStabilizingLoop(1.0);
+        openServoDropper();
+        initTime = getCurTime();
+        simpleStabilizingLoop(1.5);
+        closeServoDropper();
+    }
+}
