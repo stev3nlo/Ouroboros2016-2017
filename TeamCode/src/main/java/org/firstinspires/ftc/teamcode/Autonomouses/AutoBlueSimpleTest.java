40743690e0414177e2b265c1701b4e677dd8bc73
mnanez26 <mormorswim@gmail.com>
2017-01-12 08:32:46 -0600

    Changed encoder distances

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomouses/AutoBlueSimpleTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomouses/AutoBlueSimpleTest.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode.Autonomouses;
 
+import org.firstinspires.ftc.teamcode.Libraries.DiagnosticLibrary;
+import org.firstinspires.ftc.teamcode.Libraries.DiagnosticQuery;
 import org.firstinspires.ftc.teamcode.Libraries.MyAutonomous;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
@@ -8,6 +10,14 @@ import org.firstinspires.ftc.teamcode.Libraries.MyAutonomous;
 import org.firstinspires.ftc.teamcode.Libraries.MyOpMode;
 import org.firstinspires.ftc.teamcode.Libraries.SensorMRColor;
 import org.firstinspires.ftc.teamcode.Libraries.SensorMRRange;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 
 
 /**
@@ -23,6 +33,46 @@ public class AutoBlueSimpleTest extends MyAutonomous {
     //press far beacon
     //back up until at closest beacon using color sensor
     //press beacon
+    DiagnosticLibrary dl;
+
+    public void initializeDiagnosticLibrary()  throws IOException, ClassNotFoundException
+    {
+        try{
+            FileInputStream fis = new FileInputStream("/sdcard/FIRST/DiagnosticLibraryStorage.txt");
+            ObjectInputStream ois = new ObjectInputStream(fis);
+            //out.println("hello");
+            dl = (DiagnosticLibrary) ois.readObject();
+            fis.close();
+        }
+        catch (FileNotFoundException fnes)
+        {
+            try{
+                File file = new File("/sdcard/FIRST/DiagnosticLibraryStorage.txt");
+                FileOutputStream fos = new FileOutputStream(file);
+                dl = new DiagnosticLibrary();
+                ObjectOutputStream oos = new ObjectOutputStream(fos);
+                oos.writeObject(dl);
+                oos.flush();
+                oos.close();
+            }
+            catch(FileNotFoundException fnfe)
+            {
+
+            }
+            catch(IOException ioe)
+            {
+
+            }
+        }
+        catch(IOException ioe)
+        {
+
+        }
+        catch(ClassNotFoundException cnfe)
+        {
+
+        }
+    }
 
 
     public void runOpMode() throws InterruptedException {
@@ -30,6 +80,7 @@ public class AutoBlueSimpleTest extends MyAutonomous {
         //rangeB.initializeSensors();
         //initializeBlueServos();
         super.runOpMode();
+        //try{initializeDiagnosticLibrary();}catch (IOException ioe){} catch(ClassNotFoundException cnfe){}
         //initializeSensors();
         double startAngle = gyro.getYaw();
         double yawDiff = 0.0;
@@ -46,32 +97,35 @@ public class AutoBlueSimpleTest extends MyAutonomous {
         initCurtime();
         double startTime = getCurTime();
         double batteryLevel = hardwareMap.voltageSensor.get("Motor Controller 2").getVoltage();
-        double adjustedBatteryLevel = (int)(batteryLevel * 100);
-
-        if (batteryLevel > 13.0)
-            runSpinner(0.82);
-        else
-            runSpinner(0.92);
+        //int adjustedBatteryLevel = (int)(batteryLevel * 100);
+        //DiagnosticQuery dq = new DiagnosticQuery("motorSpinner",adjustedBatteryLevel,2.285);
+        //double powerToSend = dl.getSpeedForDiagnosticQuery(dq);
+        //runSpinner(powerToSend);
+        //if (batteryLevel > 13.0)
+        //    runSpinner(0.82);
+        //else
+        //    runSpinner(0.92);
+        runSpinner(0.84);
         pause(0.1);
-        moveWithEncoders(0.32, 3600);
+        moveWithEncoders(0.32, 2700);
         pause(0.1);
         openServoDropper();
         pause(1.5);
         closeServoDropper();
         runSpinner(0.0);
         pause(0.25);
-        moveWithEncoders(0.25, 3300);
-
-        //moveWithEncoders(.5, 1000);
+        moveWithEncoders(0.25, 1200);
         gyroArcTurnRight(0.2, yawDiff - 13.0);
-        while(opModeIsActive())
+
+        while (opModeIsActive())
         {
             telemetry.addData("USDF",rangeF.getUltraSonicDistance());
             telemetry.addData("USDB",rangeB.getUltraSonicDistance());
             telemetry.update();
-            idle();
         }
         /*
+        //moveWithEncoders(.5, 1000);
+
         pause(0.1);
         turnParallelToWallWithGyroSimple(0.195, 0);
         //17 max
@@ -105,7 +159,7 @@ public class AutoBlueSimpleTest extends MyAutonomous {
         //stabilizeAlongWall(-0.16, 4.0, 11, true, true, 2000);
         moveWithEncoders(-0.25,2000);
         pause(0.1);
-        driveAlongWallToBeacon(-0.125,true);
+        driveAlongWallToBeacon(-0.125, true);
         pause(0.1);
         pushButton();
 
