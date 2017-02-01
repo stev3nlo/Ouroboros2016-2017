4a626af387e1a0f011dcc0493ada4731a4484673
Spencer Sharp <spencersharp@me.com>
2017-02-01 08:00:48 -0600

    Added AutoRedRollers

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomouses/AutoRedRollers.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomouses/AutoRedRollers.java
@@ -13,8 +13,8 @@ import org.firstinspires.ftc.teamcode.Libraries.SensorMRRange;
 /**
  * Created by Steven on 11/14/2016.
  */
-@Autonomous(name="Auto Blue Rollers", group="Autonomous")
-public class AutoBlueRollers extends MyAutonomous {
+@Autonomous(name="Auto Red Rollers", group="Autonomous")
+public class AutoRedRollers extends MyAutonomous {
     //travel 3ft to preferred shooting place
     //shoot
     //travel across the field to far beacon
@@ -36,6 +36,7 @@ public class AutoBlueRollers extends MyAutonomous {
         while(!opModeIsActive() && !isStopRequested())
         {
             yawDiff = getAngleDiff(startAngle,gyro.getYaw());
+            //telemetry.addData("yawDiff",yawDiff);
             boolean isGyroGood = yawDiff < 60.0;
             telemetry.addData("isGyroGood",isGyroGood);
             telemetry.update();
@@ -46,32 +47,33 @@ public class AutoBlueRollers extends MyAutonomous {
         double startTime = getCurTime();
         double batteryLevel = hardwareMap.voltageSensor.get("Motor Controller 2").getVoltage();
         if(batteryLevel > 13.0)
-            runSpinner(0.8);
+            runSpinner(0.83);
         else
             runSpinner(0.88);
         pause(0.1);
-        moveWithEncodersCoast(.3, 2100, 1.0, 1.0);
+        moveWithEncodersCoast(-.35, 1280, 1.0, 1);
         pause(0.1);
         openServoDropper();
         pause(1.5);
         closeServoDropper();
         runSpinner(0.0);
         pause(1.0);
-        moveWithEncodersCoast(0.22, 1500, 1.0, 1);
+        moveWithEncodersCoast(-0.22, 2170, 1.0, 1);
 
-        gyroArcTurnRight(0.2, yawDiff - 3);
+
+        //moveWithEncoders(.5, 1000);
+        gyroArcTurnRight(-0.26, yawDiff -3);
 
         moveWithEncodersCoast(0.22, 3500, 1.0, 0.7);
 
-        driveAlongWallToBeacon(-0.13, true, 1.0, 0.7);
+        driveAlongWallToBeacon(-.15, false,1.0, 0.7);
 
         pushButtonWithRollers();
+
         pause(0.5);
 
         driveToNextBeacon(-0.25,false,2000,1.0,0.7);
-
-        pushButtonWithRollers();
-
         pause(0.5);
+        pushButtonWithRollers();
     }
 }
